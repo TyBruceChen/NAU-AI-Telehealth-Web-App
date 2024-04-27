00b9785ad36ff50a28d857cb05b9d8436367f479
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-04-27 00:29:32 -0700

    Add files via upload

--- text_modification.py
+++ text_modification.py
@@ -1,13 +1,12 @@
 
 class text_modification():
-    def __init__(self,replace_text_path = 'templates/index.html',replaced_element = 'Test Page'):
-        self.replaced_element = replaced_element
+    def __init__(self,replace_text_path = 'templates/index.html'):
         with open (replace_text_path,'r') as f:
             self.content = f.read()
 
-    def replace_content(self,new_element):
-        self.content = self.content.replace(self.replaced_element, new_element)
-        self.replaced_element = new_element
+    def replace_content(self,new_element,replaced_element = 'Test Page'):
+        self.content = self.content.replace(replaced_element, new_element)
+        #self.replaced_element = new_element
         return self.content
     
 def lung_type(cls_num,ls = ['COVID-19','Lung-Opacity','Normal','Pneumonia']):
